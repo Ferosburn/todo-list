a2f4f5eace6a0b84ab60c1ec9318f0e962053307
Ferosburn <udi.khoirul@gmail.com>
2022-06-20 12:28:33 +0700

    add edit feature

--- src/Data.js
+++ src/Data.js
@@ -1,12 +1,35 @@
 import React, { useState } from "react";
 
 function Data(props) {
+  const [item, setItem] = useState(props.item);
   const [check, setCheck] = useState('');
   const [finished, setFinished] = useState('');
   const [hideItem, setHideItem] = useState('');
+  const [update, setUpdate] = useState(false);
+  const [updateItem, setUpdateItem] = useState(item);
+  let edit;
+  if(update) {
+    edit = (
+      <form onSubmit={()=>{handleSubmit()}}>
+        <input value={updateItem} onChange={(e)=>{handleChange(e)}} />
+        <button type="submit">update</button>
+      </form>
+    )
+  }
+  const handleSubmit = () => {
+    setItem(updateItem);
+    setUpdate(false);
+    setUpdateItem(item)
+  }
+  const handleChange = (e) => {
+    setUpdateItem(e.target.value)
+  }
+  const handleEdit = () => {
+    setUpdate(true);
+  }
   const handleCheck = () => {
     if(check === '') {
-      setCheck('V');
+      setCheck((<strong className="check-icon">V</strong>));
       setFinished('finished')
     } else {
       setCheck('');
@@ -20,7 +43,8 @@ function Data(props) {
   return (
     <div className={`item ${finished} ${hideItem}`}>
       <button className="checklist" onClick={()=>{handleCheck()}}>{check}</button>
-      <span>{props.item}{props.id}</span>
+      <span>{item}{edit}</span>
+      <button onClick={()=>{handleEdit()}}>edit</button>
       <button onClick={()=>{handleDelete()}}>X</button>
     </div>
   )
